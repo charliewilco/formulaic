b57a1fce0d0471ed6da0bc2394d106644be6bbcf
Charlie ⚡ <charliewilc0@protonmail.com>
2022-10-21 07:51:54 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,129 @@
+import { useForm } from "./useForm";
+import z from "zod";
+
+const validationSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(8),
+});
+
+function App() {
+  const { errors, getFieldProps, getFormProps, getErrorProps } = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit(values) {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img
+          className="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" {...getFormProps()}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  {...getFieldProps("email")}
+                />
+                {errors["email"] && (
+                  <p
+                    className="text-red-500 text-xs italic"
+                    {...getErrorProps("email")}
+                  >
+                    {errors["email"]}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  {...getFieldProps("password")}
+                />
+                {errors["password"] && (
+                  <p
+                    className="text-red-500 text-xs italic"
+                    {...getErrorProps("password")}
+                  >
+                    {errors["password"]}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test/basic.test.tsx
@@ -0,0 +1,47 @@
+import { describe, test, expect, vi } from "vitest";
+
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+import App from "../App";
+
+const IntersectionObserverMock = vi.fn(() => ({
+	disconnect: vi.fn(),
+	observe: vi.fn(),
+	takeRecords: vi.fn(),
+	unobserve: vi.fn(),
+}));
+
+vi.stubGlobal("IntersectionObserver", IntersectionObserverMock);
+
+describe("Todo List", () => {
+	test("setup", async () => {
+		render(<App />);
+
+		expect(screen.queryAllByRole("listitem")).toHaveLength(2);
+	});
+
+	test("add todo", async () => {
+		render(<App />);
+
+		await userEvent.click(screen.getByLabelText("Add New"));
+
+		expect(screen.getByRole("dialog")).toBeInTheDocument();
+		const input = screen.getByRole("textbox");
+		await userEvent.type(input, "Eat a snack");
+
+		await userEvent.click(screen.getByLabelText("Submit"));
+		expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+
+		expect(screen.queryAllByRole("listitem")).toHaveLength(3);
+	});
+
+	test("remove todo", async () => {
+		render(<App />);
+
+		await userEvent.click(screen.queryAllByLabelText("Remove Todo")[0]);
+
+		expect(screen.queryAllByRole("listitem")).toHaveLength(1);
+	});
+});
